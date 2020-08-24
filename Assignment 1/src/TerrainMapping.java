2fb71aa59ea6e79601c29427926e77909a51543b
DylanRees07 <Dylanrees07@gmail.com>
2020-08-25 00:37:23 +0200

    Created dummy arrays to test on

--- Assignment 1/src/TerrainMapping.java
+++ Assignment 1/src/TerrainMapping.java
@@ -3,13 +3,24 @@ import java.util.concurrent.ForkJoinPool;
 
 public class TerrainMapping {
 	
+	static double[][] terrain;
+	static int[][] classified;
+	
 	
 	public static void main(String[] args) {
 		
+		terrain = new double[3][3];
+		classified = new int[3][3];
 		
+		for (int i = 0; i < terrain.length; i++) {
+			
+			for (int j = 0; j < terrain[i].length; j++) {
+				terrain[i][j] = 3.4;
+			}
+		}
 		
-		
-		
+		terrain[1][1] = 3.2;
+			
 	}
 
 }
